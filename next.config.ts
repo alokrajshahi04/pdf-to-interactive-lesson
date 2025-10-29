ff22f91c001f48a0898908000361b054af2cd365
sam <sam.barrowclough@gmail.com>
2025-10-29 21:03:37 +0000

    conf

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ["sharp", "zerox"],
-  // Add empty turbopack config to silence warning and use default Turbopack
-  turbopack: {},
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Mark sharp as external
-      config.externals.push("sharp");
-    }
-
-    // Ignore Tesseract worker files that cause issues in serverless
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // Mock tesseract.js worker to prevent it from loading
-      // We don't need it since we're using a custom vision model
-      "tesseract.js/src/worker-script/node/index.js": false,
-    };
-
-    return config;
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["tesseract.js", "zerox", "sharp"],
+    outputFileTracingIncludes: {
+      "/api/**/*": ["./node_modules/**/*.wasm", "./node_modules/**/*.proto"],
+    },
   },
 };
-
-export default nextConfig;
