dfe9d8282e8c0734d29348e0896925d7aa50b8ba
sam <sam.barrowclough@gmail.com>
2025-11-05 19:28:37 +0000

    fix build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig = {
   outputFileTracingIncludes: {
     "/api/**/*": ["./node_modules/**/*.wasm", "./node_modules/**/*.proto"],
   },
+  turbopack: {
+    root: __dirname,
+  },
 } as NextConfig;
 
 export default nextConfig;
